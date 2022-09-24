0ac67d7229419e72e62962e29f54b3c9002b0969
Oluwadamilare Emmanuel Akerele <56739109+Drhay1@users.noreply.github.com>
2022-09-24 15:31:42 +0300

    update

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ function digitalClock(){
     const date = new Date();
     const H = date.getHours();
     const M = date.getMinutes();
+    const Y = date.getFullYear();
+    const D = date.getDay();
+    const Mo = date.getMonth();
     let S = date.getSeconds();
+
 
     let EvMo  = 'PM';
     if(H < 12){
@@ -10,10 +14,12 @@ function digitalClock(){
     }
 
     const time = document.getElementById('time');
+    const year = document.getElementById('year')
 
     S = S < 10 ? '0'+S : S;
     
     time.innerText = H+':'+M+':'+S +' '+EvMo;
+    year.innerText = D+ ':'+ Mo+ ':'+Y;
 }
 
 setInterval(digitalClock, 1000);
